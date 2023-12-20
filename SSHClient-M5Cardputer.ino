af692a617f9804be0cd6b86abf1f25f664521e32
aat440hz <99800093+aat440hz@users.noreply.github.com>
2023-12-19 21:39:31 -0800

    Update SSHClient-M5Cardputer.ino

--- SSHClient-M5Cardputer.ino
+++ SSHClient-M5Cardputer.ino
@@ -3,64 +3,176 @@
 #include "libssh_esp32.h"
 #include <libssh/libssh.h>
 
-const char* ssid = "Your_SSID";
-const char* password = "Your_Password";
+const char* ssid = "SETUP-8CD3"; // Replace with your WiFi SSID
+const char* password = "career6174brace"; // Replace with your WiFi password
+
+// SSH server configuration
+const char* ssh_host = "192.168.0.188"; // Replace with your SSH server address
+const char* ssh_user = "user"; // Replace with your SSH username
+const char* ssh_password = "u53r"; // Replace with your SSH password
+
+// M5Cardputer setup
+M5Canvas canvas(&M5Cardputer.Display);
+String commandBuffer = "> ";
+int cursorY = 0;
+const int lineHeight = 8;
+unsigned long lastKeyPressMillis = 0;
+const unsigned long debounceDelay = 200; // Adjust debounce delay as needed
 
 // Function declarations
 ssh_session connect_ssh(const char *host, const char *user, int verbosity);
 int authenticate_console(ssh_session session, const char *password);
-int verify_knownhost(ssh_session session);
-
-// SSH Task Declaration
 void sshTask(void *pvParameters);
 
 void setup() {
-    M5.begin(); 
-    M5.Lcd.setRotation(1);
-    M5.Lcd.print("Connecting to WiFi...\n");
+    auto cfg = M5.config();
+    M5Cardputer.begin(cfg, true);
+    M5Cardputer.Display.setRotation(1);
+    M5Cardputer.Display.setTextSize(1); // Set text size
 
+    // Connect to WiFi
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
-        M5.Lcd.print(".");
     }
 
-    M5.Lcd.println("\nWiFi connected.");
-    M5.Lcd.print("IP Address: ");
-    M5.Lcd.println(WiFi.localIP());
-
+    // Connect to SSH server
     TaskHandle_t sshTaskHandle = NULL;
-    xTaskCreatePinnedToCore(sshTask, "SSH Task", 20000, NULL, 1, &sshTaskHandle, 1); // Increased stack size
-
+    xTaskCreatePinnedToCore(sshTask, "SSH Task", 20000, NULL, 1, &sshTaskHandle, 1);
     if (sshTaskHandle == NULL) {
         Serial.println("Failed to create SSH Task");
     }
+
+    // Initialize the cursor Y position
+    cursorY = M5Cardputer.Display.getCursorY();
 }
 
 void loop() {
-    M5.update();
-    // The loop does not perform any operation in this case.
+    M5Cardputer.update();
 }
 
 void sshTask(void *pvParameters) {
-    ssh_session my_ssh_session = connect_ssh("192.168.0.188", "user", SSH_LOG_PROTOCOL);
-    if (my_ssh_session != NULL) {
-        M5.Lcd.println("SSH Connection established.");
-
-        if (authenticate_console(my_ssh_session, "password") == SSH_OK) {
-            M5.Lcd.println("SSH Authentication succeeded.");
-            // Authentication successful
-        } else {
-            M5.Lcd.println("SSH Authentication failed.");
-        }
-
-        ssh_disconnect(my_ssh_session);
-        ssh_free(my_ssh_session);
-    } else {
-        M5.Lcd.println("SSH Connection failed.");
+    ssh_session my_ssh_session = connect_ssh(ssh_host, ssh_user, SSH_LOG_PROTOCOL);
+    if (my_ssh_session == NULL) {
+        M5Cardputer.Display.println("SSH Connection failed.");
+        vTaskDelete(NULL);
+        return;
     }
 
-    vTaskDelete(NULL); // Delete the task when done
+    M5Cardputer.Display.println("SSH Connection established.");
+    if (authenticate_console(my_ssh_session, ssh_password) != SSH_OK) {
+        M5Cardputer.Display.println("SSH Authentication failed.");
+        ssh_disconnect(my_ssh_session);
+        ssh_free(my_ssh_session);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    M5Cardputer.Display.println("SSH Authentication succeeded.");
+
+    // Open a new channel for the SSH session
+    ssh_channel channel = ssh_channel_new(my_ssh_session);
+    if (channel == NULL || ssh_channel_open_session(channel) != SSH_OK) {
+        M5Cardputer.Display.println("SSH Channel open error.");
+        ssh_disconnect(my_ssh_session);
+        ssh_free(my_ssh_session);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    // Request a pseudo-terminal
+    if (ssh_channel_request_pty(channel) != SSH_OK) {
+        M5Cardputer.Display.println("Request PTY failed.");
+        ssh_channel_close(channel);
+        ssh_channel_free(channel);
+        ssh_disconnect(my_ssh_session);
+        ssh_free(my_ssh_session);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    // Start a shell session
+    if (ssh_channel_request_shell(channel) != SSH_OK) {
+        M5Cardputer.Display.println("Request shell failed.");
+        ssh_channel_close(channel);
+        ssh_channel_free(channel);
+        ssh_disconnect(my_ssh_session);
+        ssh_free(my_ssh_session);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    while (true) {
+        M5Cardputer.update();
+
+        // Handle keyboard input with debounce
+        if (M5Cardputer.Keyboard.isChange()) {
+            if (M5Cardputer.Keyboard.isPressed()) {
+                unsigned long currentMillis = millis();
+                if (currentMillis - lastKeyPressMillis >= debounceDelay) {
+                    Keyboard_Class::KeysState status = M5Cardputer.Keyboard.keysState();
+
+                    for (auto i : status.word) {
+                        commandBuffer += i;
+                        M5Cardputer.Display.print(i); // Display the character as it's typed
+                        cursorY = M5Cardputer.Display.getCursorY(); // Update cursor Y position
+                    }
+
+                    if (status.del && commandBuffer.length() > 2) {
+                        commandBuffer.remove(commandBuffer.length() - 1);
+                        M5Cardputer.Display.setCursor(M5Cardputer.Display.getCursorX() - 6, M5Cardputer.Display.getCursorY());
+                        M5Cardputer.Display.print(" "); // Print a space to erase the last character
+                        M5Cardputer.Display.setCursor(M5Cardputer.Display.getCursorX() - 6, M5Cardputer.Display.getCursorY());
+                        cursorY = M5Cardputer.Display.getCursorY(); // Update cursor Y position
+                    }
+
+                    if (status.enter) {
+                        String message = commandBuffer.substring(2) + "\r\n"; // Use "\r\n" for newline
+                        ssh_channel_write(channel, message.c_str(), message.length()); // Send message to SSH server
+
+                        commandBuffer = "> ";
+                        M5Cardputer.Display.print('\n'); // Move to the next line
+                        cursorY = M5Cardputer.Display.getCursorY(); // Update cursor Y position
+                    }
+
+                    lastKeyPressMillis = currentMillis;
+                }
+            }
+        }
+
+        // Check if the cursor has reached the bottom of the display
+        if (cursorY > M5Cardputer.Display.height() - lineHeight) {
+            // Scroll the display up by one line
+            M5Cardputer.Display.scroll(0, -lineHeight);
+
+            // Reset the cursor to the new line position
+            cursorY -= lineHeight;
+            M5Cardputer.Display.setCursor(M5Cardputer.Display.getCursorX(), cursorY);
+        }
+
+        char buffer[1024];
+        int nbytes = ssh_channel_read_nonblocking(channel, buffer, sizeof(buffer), 0);
+        if (nbytes > 0) {
+            for (int i = 0; i < nbytes; ++i) {
+                if (buffer[i] == '\r') {
+                    continue; // Handle carriage return
+                }
+                M5Cardputer.Display.write(buffer[i]);
+                cursorY = M5Cardputer.Display.getCursorY(); // Update cursor Y position
+            }
+        }
+
+        if (nbytes < 0 || ssh_channel_is_closed(channel)) {
+            break;
+        }
+    }
+
+    // Clean up
+    ssh_channel_close(channel);
+    ssh_channel_free(channel);
+    ssh_disconnect(my_ssh_session);
+    ssh_free(my_ssh_session);
+    vTaskDelete(NULL);
 }
 
 ssh_session connect_ssh(const char *host, const char *user, int verbosity) {
@@ -91,10 +203,5 @@ int authenticate_console(ssh_session session, const char *password) {
         Serial.println(ssh_get_error(session));
         return rc;
     }
-    return SSH_OK;
-}
-
-int verify_knownhost(ssh_session session) {
-    // Placeholder implementation for host verification
     return SSH_OK;
 }
